c242c2c0e074f8f53dda60f382545a4a5bc646bf
fanelu <lm76571@gmail.com>
2024-03-03 18:32:32 +0200

    Start frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,35 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Book } from './book';
-import { BookService } from './book.service';
-import { response } from 'express';
-import { error } from 'console';
-import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
-
-export class AppComponent implements OnInit{
-  title = "frontend";
-  public books: Book[] = [];
-
-  constructor(private bookServie: BookService){}
-
-  ngOnInit(): void {
-      this.getBooks();
-  }
-
-  public getBooks(): void{
-    this.bookServie.getBooks().subscribe(
-      (response: Book[]) => {
-        this.books = response;
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    )
-  }
+export class AppComponent {
+  title = 'frontend';
 }
